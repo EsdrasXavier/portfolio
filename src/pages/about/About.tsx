9eaef9106025dcde11135c1ea10f35f4ce831e2d
Esdras Xavier <esdrasxa@gmail.com>
2021-05-30 20:41:03 -0300

    Changed about layout.

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,21 +1,24 @@
 import { Parallax } from 'react-parallax';
 import Typing from 'react-typing-animation';
+import { StyledAbout } from './style';
 
 const About: React.FC = () => {
   return (
     <Parallax>
-      <div style={{ height: 500 }}>
-        <Typing speed={20}>
-          <div>
-            Hi, welcome to my website! My name is Esdras Xavier and I'm a full
-            stack developer.
-          </div>
-          <div>
-            One of my 2021 goals is to become expert in Java with Spring Boot
-            and microservices.
-          </div>
-        </Typing>
-      </div>
+      <StyledAbout>
+        <div style={{ height: 500 }}>
+          <Typing speed={20}>
+            <div>
+              Hi, welcome to my website! My name is Esdras Xavier and I'm a full
+              stack developer.
+            </div>
+            <div>
+              One of my 2021 goals is to become expert in Java with Spring Boot
+              and microservices.
+            </div>
+          </Typing>
+        </div>
+      </StyledAbout>
     </Parallax>
   );
 };
---
+++ src/pages/about/style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const StyledAbout = styled.div`
+  height: 100vh;
+  padding-top: 10vh;
+`;
